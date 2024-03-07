4962da603e84f32d3da5b9cdac171b505a9eac76
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-03-07 11:54:40 +0000

    add spinner to logout

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import Spinner from '@/components/Spinner';
+import { Grid } from '@radix-ui/themes';
 import { signOut } from 'next-auth/react';
 import { useEffect } from 'react';
 
@@ -9,7 +11,17 @@ const LogoutPage = () => {
       callbackUrl: '/',
     });
   });
-  return <></>;
+
+  return (
+    <>
+      <Grid
+        columns='1'
+        className='h-screen place-content-center place-items-center'
+      >
+        <Spinner />
+      </Grid>
+    </>
+  );
 };
 
 export default LogoutPage;
