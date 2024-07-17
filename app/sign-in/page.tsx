8504a4f074e6858085818c3f5404aab52e2fb7df
Sayed Hamid Fatimi <sayedhfatimi@pm.me>
2024-07-17 13:57:11 +0100

    fix SignInPage

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,6 @@
 import { signIn } from "@/lib/auth"
  
-<<<<<<< HEAD
 const SignInPage = () => {
-=======
-export function SignIn() {
->>>>>>> 81b0658d1cd029a420776d47ce2b7eb07ce5f1dc
   return (
     <form
       action={async (formData) => {
@@ -16,10 +12,6 @@ export function SignIn() {
       <button type="submit">Signin with Resend</button>
     </form>
   )
-<<<<<<< HEAD
 }
 
 export default SignInPage;
-=======
-}
->>>>>>> 81b0658d1cd029a420776d47ce2b7eb07ce5f1dc
