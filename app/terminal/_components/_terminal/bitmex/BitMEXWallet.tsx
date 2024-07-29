9238d1a6d005f348e91854c59b673f1a272f8628
Sayed Hamid Fatimi <sayedhfatimi@pm.me>
2024-07-29 15:03:41 +0100

    build BitMEXWallet component

--- app/terminal/_components/_terminal/bitmex/BitMEXWallet.tsx
+++ app/terminal/_components/_terminal/bitmex/BitMEXWallet.tsx
@@ -1,4 +1,10 @@
 import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import { ICON_SIZE_SMALL } from '@/lib/consts/UI';
+import {
   TABLE_NAME_WALLET,
   symbolSignificantFiguresMap,
 } from '@/lib/consts/terminal/bitmex';
@@ -7,6 +13,7 @@ import useBitmexWs from '@/lib/hooks/useBitmexWs';
 import type { TWallet } from '@/lib/types/BitmexDataTypes';
 import Image from 'next/image';
 import { useEffect } from 'react';
+import { LuChevronDown } from 'react-icons/lu';
 
 const BitMEXWallet = () => {
   const { data, sendJsonMessage } = useBitmexWs<TWallet>(TABLE_NAME_WALLET);
@@ -26,30 +33,77 @@ const BitMEXWallet = () => {
   }, [sendJsonMessage]);
 
   return (
-    <div className='flex flex-col'>
+    <>
       {data.map((asset) => (
-        <div
-          key={asset.currency}
-          className='flex flex-row items-center justify-between px-1 py-2 hover:bg-secondary'
-        >
-          <div className='flex flex-row items-center space-x-2'>
-            <Image
-              src={CryptoIconMap[asset.currency.toLowerCase()]}
-              height={16}
-              width={16}
-              alt={asset.currency}
-            />
-            <span>{asset.currency}</span>
-          </div>
-          <div>
-            {(
-              asset.amount /
-              10 ** symbolSignificantFiguresMap[asset.currency.toLowerCase()]
-            ).toFixed(8)}
-          </div>
-        </div>
+        <Collapsible key={asset.currency}>
+          <CollapsibleTrigger asChild>
+            <div className='group flex w-full flex-row items-center justify-between px-1 py-2 transition-all hover:bg-secondary'>
+              <div className='flex flex-row items-center space-x-2'>
+                <Image
+                  src={CryptoIconMap[asset.currency.toLowerCase()]}
+                  height={16}
+                  width={16}
+                  alt={asset.currency}
+                />
+                <span>{asset.currency}</span>
+              </div>
+
+              <div className='flex flex-row items-center justify-end space-x-2'>
+                <span>
+                  {(
+                    asset.amount /
+                    10 **
+                      symbolSignificantFiguresMap[asset.currency.toLowerCase()]
+                  ).toFixed(8)}
+                </span>
+                <LuChevronDown
+                  size={ICON_SIZE_SMALL}
+                  className='shrink-0 transition-transform duration-200 group-data-[state=open]:rotate-180'
+                />
+              </div>
+            </div>
+          </CollapsibleTrigger>
+          <CollapsibleContent asChild>
+            <div className='flex flex-col space-y-2 px-2 py-1'>
+              <div className='flex flex-row items-center justify-between'>
+                <span className='text-muted-foreground text-xs'>Available</span>
+                <span className='mr-5'>
+                  {(
+                    asset.amount /
+                    10 **
+                      symbolSignificantFiguresMap[asset.currency.toLowerCase()]
+                  ).toFixed(8)}
+                </span>
+              </div>
+              <div className='flex flex-row items-center justify-between'>
+                <span className='text-muted-foreground text-xs'>
+                  Pending Deposit
+                </span>
+                <span className='mr-5'>
+                  {(
+                    asset.pendingCredit /
+                    10 **
+                      symbolSignificantFiguresMap[asset.currency.toLowerCase()]
+                  ).toFixed(8)}
+                </span>
+              </div>
+              <div className='flex flex-row items-center justify-between'>
+                <span className='text-muted-foreground text-xs'>
+                  Pending Withdrawal
+                </span>
+                <span className='mr-5'>
+                  {(
+                    asset.pendingDebit /
+                    10 **
+                      symbolSignificantFiguresMap[asset.currency.toLowerCase()]
+                  ).toFixed(8)}
+                </span>
+              </div>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
       ))}
-    </div>
+    </>
   );
 };
 
